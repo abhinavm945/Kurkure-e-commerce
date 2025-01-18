72dc375cac7bc9aaaa1ae716bc15a807f3e41192
devagya <rattandevagya@gmail.com>
2025-01-18 17:30:12 +0530

    machine learning

---
+++ Machine_Learning/Postgres_data.py
@@ -0,0 +1,29 @@
+import psycopg2
+import pandas as pd
+import os
+from dotenv import load_dotenv, dotenv_values 
+load_dotenv() 
+def fetch_orders():
+    # Database connection with SSL
+    conn = psycopg2.connect(
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT"),
+        database=os.getenv("DATABASE"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        sslmode=os.getenv("SSLMODE"),
+    )
+
+    # Query to fetch orders
+    query = """
+SELECT "userId","cartId" FROM "Order";
+"""
+    df = pd.read_sql(query, conn)
+
+    # Close the connection
+    conn.close()
+    return df
+
+if __name__ == "__main__":
+    order = fetch_orders()
+    print(order.head())
